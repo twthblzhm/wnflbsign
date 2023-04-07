34709c18d02a15279d2e48cd0467985f748514a0
twthblzhm <twthblzhm@126.com>
2023-04-08 02:42:26 +0800

    Update autosing.py

--- autosing.py
+++ autosing.py
@@ -14,20 +14,29 @@ from sendNotify import send
 def start(cookie, username):
     try:
         s = requests.session()
-
-        flb_url = get_addr()
-        headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                   'Accept - Encoding': 'gzip, deflate',
-                   'Accept-Language': 'zh-CN,zh;q=0.9',
-                   'cache-control': 'max-age=0',
-                   'Host': flb_url,
-                   'Upgrade-Insecure-Requests': '1',
-                   'Cookie': cookie,
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62'}
-
+        flb_url = 'www.wnflb2023.com'
+        #print(flb_url)
+        #print(cookie)
+        headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                   'accept-encoding': 'gzip, deflate, br',
+                   'accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
+                   'cookie': cookie,
+                   'dnt': '1',
+                   'sec-ch-ua': '"Microsoft Edge";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+                   'sec-ch-ua-mobile': '?0',
+                   'sec-ch-ua-platform': "Windows",
+                   'sec-fetch-dest': 'document',
+                   'sec-fetch-mode': 'navigate',
+                   'sec-fetch-site': 'none',
+                   'sec-fetch-user': '?1',
+                   'upgrade-insecure-requests': '1',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.62'}
         # 访问Pc主页
-        user_info = s.get('https://' + flb_url + '/forum.php?mobile=no', headers=headers).text
+        user_info = s.get('https://' + flb_url + '/forum-2-1.html', headers=headers,verify=False).text
+        #print(user_info)
+        #print(username)
         user_name = re.search(r'title="访问我的空间">(.*?)</a>', user_info)
+        #print(user_name)
         if user_name:
             print("登录用户名为：" + user_name.group(1))
             print("环境用户名为：" + username)
@@ -49,18 +58,18 @@ def start(cookie, username):
         sing_day = re.search(r'<div class="tip_c">(.*?)</div>', user_info).group(1)
         log_info = "{}当前{}".format(sing_day, current_money)
         print(log_info)
-        send("签到结果", log_info)
+        #send("签到结果", log_info)
 
     except Exception as e:
         print("签到失败，失败原因:"+str(e))
-        send("签到结果", str(e))
+        #send("签到结果", str(e))
 
 
 def get_addr():
-    pub_page = "https://fuliba-1251744788.file.myqcloud.com"
+    pub_page = "http://fuliba2023-1256179406.file.myqcloud.com/"
     ret = requests.get(pub_page)
     ret.encoding = "utf-8"
-    bbs_addr = re.findall(r'<a href=.*?><i>https://(.*?)</i></a>', ret.text)[1]
+    bbs_addr = re.findall(r'福利吧论坛地址<a href=.*?><i>https://(.*?)</i></a>', ret.text)[1]
     return bbs_addr
 
 
